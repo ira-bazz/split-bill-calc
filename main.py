58ea32e6873ca8d73ddfa2bfef093644b3eb0bde
Irina Ermiychuk <irinaermiychuk@gmail.com>
2023-05-09 19:28:09 +0300

    first commit

--- main.py
+++ main.py
@@ -12,3 +12,5 @@ random_choice = random.randint(0, num_items - 1)
 # Pick out random person from list of names using the random number.
 person_who_will_pay = names[random_choice]
 print(person_who_will_pay + " is going to buy a meal today!")
+
+#
